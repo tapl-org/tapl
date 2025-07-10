04df8eb6e31cf7ea492f9730b5c10dafbc4f901a
Orti Bazar <orti.bazar@gmail.com>
2025-07-10 10:16:36 -0700

    rename grammar to rule_names

--- python/tapl-lang/tests/pythonlike/rule_names.py
+++ python/tapl-lang/tests/pythonlike/rule_names.py
@@ -3,16 +3,16 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 
-from tapl_lang.pythonlike import grammar
+from tapl_lang.pythonlike import rule_names
 
 
 def test_rule_name_consistency():
-    assert grammar.RULE_NAMES, 'Grammar rule names are not define or empty.'
-    for full_rule_name in grammar.RULE_NAMES:
+    assert rule_names.RULE_NAMES, 'Grammar rule names are not define or empty.'
+    for full_rule_name in rule_names.RULE_NAMES:
         names = full_rule_name.split('.')
         assert len(names) == 2, f'Rule name {full_rule_name} does not have exactly two parts.'
         assert names[0] == 'pythonlike', f'Rule name {full_rule_name} does not start with "pythonlike".'
         constant_name = names[1].upper()
         assert (
-            constant_name in grammar.__dict__
+            constant_name in rule_names.__dict__
         ), f'Rule name {constant_name} is not found as a constant in the grammar module.'
