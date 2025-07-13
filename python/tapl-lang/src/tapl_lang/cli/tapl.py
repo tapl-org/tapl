dada5797bfc063fbe3565d711b7906d3c5d1bcfc
Orti Bazar <orti.bazar@gmail.com>
2025-07-12 19:53:05 -0700

    Run tapl file

--- python/tapl-lang/src/tapl_lang/cli/tapl.py
+++ python/tapl-lang/src/tapl_lang/cli/tapl.py
@@ -14,7 +14,7 @@ from tapl_lang.core.compiler import compile_tapl
 # you can use the following command:
 # pip install -e .
 
-def compile(path: str) -> None:
+def compile_and_run(path: str) -> None:
     """
     Compiles the TAPL file at the given path.
     """
@@ -23,13 +23,13 @@ def compile(path: str) -> None:
     source = p.read_text()
     layers = compile_tapl(source)
     dir_path = os.path.dirname(absolute_path)
-    print(source)
-    for i in range(len(layers)):
+    for i in reversed(range(len(layers))):
         suffix = i if i else ''
         filename = os.path.join(dir_path, f'{p.stem}{suffix}.py')
         python_code = ast.unparse(layers[i])
         with open(filename, 'w') as f:
             f.write(python_code)
+        exec(python_code, globals())
 
 def main():
     """
@@ -39,7 +39,7 @@ def main():
     parser.add_argument('file', type=str, help='programm read from script file.')
     args = parser.parse_args()
 
-    compile(args.file)  # Call the compile function with the provided file path
+    compile_and_run(args.file)
 
 
 if __name__ == '__main__':
