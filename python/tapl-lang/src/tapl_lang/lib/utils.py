03bfdee79838037ce1a0558b5f7d3443d25fcac3
Orti Bazar <orti.bazar@gmail.com>
2025-11-23 09:31:18 -0800

    refactor: remove dynamic variable creation utility and update predef.py

--- python/tapl-lang/src/tapl_lang/lib/utils.py
+++
@@ -1,8 +0,0 @@
-# Part of the Tapl Language project, under the Apache License v2.0 with LLVM
-# Exceptions. See /LICENSE for license information.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-
-def create_dynamic_variables(namespace, variables):
-    for var_name, var_value in variables.items():
-        namespace[var_name] = var_value
--- python/tapl-lang/src/tapl_lang/pythonlike/predef.py
+++ python/tapl-lang/src/tapl_lang/pythonlike/predef.py
@@ -2,8 +2,4 @@
 # Exceptions. See /LICENSE for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from tapl_lang.lib import utils
-
-utils.create_dynamic_variables(
-    globals(), __import__('tapl_lang.pythonlike.builtin_functions', globals(), locals(), ['export']).export
-)
+globals().update(__import__('tapl_lang.pythonlike.builtin_functions', globals(), locals(), ['export']).export)
