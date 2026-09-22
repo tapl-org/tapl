faec40595e3d48f56ea59f7b53b5aa87e6ea5721
Orti Bazar <orti.bazar@gmail.com>
2026-09-22 11:29:35 -0700

    feat(oymo): update LLVM module configuration and return value
    - Set the module triple to 'aarch64-unknown-linux-gnu' for target architecture specification.
    - Changed the return constant value from 0 to 42 in the generated LLVM IR.

--- oymo/src/oymo/oymomo/llvm.py
+++ oymo/src/oymo/oymomo/llvm.py
@@ -2,8 +2,10 @@
 
 from llvmlite import ir
 
+
 # 1. Create a new module container
 module = ir.Module(name='main_module')
+module.triple = 'aarch64-unknown-linux-gnu'
 
 # 2. Define the types (32-bit integer)
 int_type = ir.IntType(32)
@@ -19,7 +21,7 @@ block = main_func.append_basic_block(name='entry')
 builder = ir.IRBuilder(block)
 
 # 6. Generate the return statement with a constant value of 0
-zero_constant = ir.Constant(int_type, 0)
+zero_constant = ir.Constant(int_type, 42)
 builder.ret(zero_constant)
 
 # 7. Print the generated LLVM IR
